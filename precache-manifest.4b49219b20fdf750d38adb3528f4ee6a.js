16b875b2477482a25f6e8472c0ee1e8707048cc3
Adi <kishek12@gmail.com>
2020-05-02 12:30:12 +1000

    Updates

---
+++ precache-manifest.4b49219b20fdf750d38adb3528f4ee6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3938cbf2a108bb834e07b6dda47609e7",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "0027a73517e7e71a27c8",
+    "url": "/home/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "122789d89d2c00a8f8d3",
+    "url": "/home/static/js/2.c40eddf8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/home/static/js/2.c40eddf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0027a73517e7e71a27c8",
+    "url": "/home/static/js/main.46dede4f.chunk.js"
+  },
+  {
+    "revision": "4b01274295bad03d6241",
+    "url": "/home/static/js/runtime-main.27f4a010.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/home/static/media/logo.5d5d9eef.svg"
+  }
+]);
