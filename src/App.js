51a0731a7e5485eb5aa0cbb196065ceb5f968db4
Adi <kishek12@gmail.com>
2020-05-18 18:09:44 +1000

    changes made

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.css";
+import aboutBg from "./static/about.jpeg";
+import contentsBg from "./static/project.jpeg";
 
 function App() {
   return (
@@ -65,60 +67,77 @@ function App() {
           >
             QUALIFICATIONS
           </div>
-          <div
-            className="header-item"
-            onClick={() => {
-              window.scroll({
-                top: window.innerHeight * 4,
-                left: 1000,
-                behavior: "smooth",
-              });
-            }}
-          >
-            CONTACT
-          </div>
         </div>
       </section>
       <section className="App-body">
         <div className="body-about">
+          <div className="body-about-img-wrapper">
+            <img src={aboutBg} alt="about bg" className="body-about-img" />
+          </div>
           <div className="body-about-description">
             <div className="body-title">ABOUT</div>
             <div className="description-text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-              metus elit, commodo at commodo non, dapibus a lorem. Maecenas
-              iaculis urna eget massa ultrices elementum. Nam sed sodales leo.
-              Aliquam erat volutpat. Praesent fermentum hendrerit mattis. Proin
-              ut turpis pharetra, tempus nisi eget, luctus lacus. Quisque
-              lacinia elit at congue dictum.
+              Hey There! My name is Adi Kishore and I'm currently a student
+              studying Software Engineering at UNSW. I'm a very passionate
+              developer who is extremely keen to jump in any problem space and
+              develop some cool solutions! I'm still in the exciting beginnings
+              of my journey but throughout my experience so far I've
+              successfully lead and executed a few projects as the full stack
+              lead, and am deeply interested to keep going!
             </div>
           </div>
         </div>
         <div className="body-contents">
+          <div className="body-contents-img-wrapper">
+            <img
+              src={contentsBg}
+              alt="contents bg"
+              className="body-contents-img"
+            />
+          </div>
           <div className="body-header">
-            <div className="body-title" style={{ "text-align": "right" }}>
-              PROJECTS
+            <div className="body-title">PROJECTS</div>
+            <div className="description-text">
+              From the beginning of 2020, I have pursued my deep interest for
+              working on projects that solve real problems in a{" "}
+              <strong>meaningful</strong> and <strong>helpful</strong> way.
             </div>
           </div>
           <div className="body-menu">
-            <MenuItem title="myHome" src="" />
             <MenuItem
               title="myLounge"
-              src="https://github.com/kishek2000/Team-100"
+              src="https://www.youtube.com/watch?v=znZc0CzMUmQ"
+              status="OPTIMISING"
+              // color="rgb(57, 240, 112)" completed colour
+              color="rgb(240, 170, 57)"
+              description="Find where to stream Movies, Shows and Music."
+              breakdown="myLounge is a digital lounge that provides users the ability to browse multiple entertainment options. It allows filtering and searching of TV Shows, Movies, Podcasts and Music and immediately provides users a list of streaming services for a chosen media option."
+              demo="Video Demo"
             />
             <MenuItem
-              title="Productive"
-              src="https://github.com/kishek2000/productive"
+              title="Discovery"
+              src="http://kishek2000.github.io/discovery"
+              status="OPTIMISING"
+              color="rgb(240, 170, 57)"
+              description="Children's education made fun and engaging."
+              demo="Live Demo"
             />
-            <MenuItem title="myBudget" src="" />
-            <MenuItem title="Discovery" src="" />
+            <MenuItem
+              title="Sadafal Education"
+              src=""
+              status="RESEARCHING"
+              color="rgb(129, 110, 237)"
+              description="Discover your path of learning, and shape your career."
+              demo="Coming Soon"
+            />
           </div>
         </div>
         <div className="body-qualifications">
           <div className="body-title">QUALIFICATIONS</div>
-        </div>
-        <div className="body-contact">
-          <div className="body-title" style={{ "text-align": "right" }}>
-            CONTACT
+          <div className="body-qualifications-row">
+            <div className="body-qualifications-subtitle">FRONTEND</div>
+            <div className="body-qualifications-subtitle">BACKEND</div>
+            <div className="body-qualifications-subtitle">FRONTEND</div>
           </div>
         </div>
       </section>
@@ -131,14 +150,31 @@ export default App;
 function MenuItem(props) {
   return (
     <div className="menu-item">
-      <a
-        href={props.src}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="link"
-      >
-        {props.title}
-      </a>
+      <div className="menu-item-text">
+        <div className="menu-item-title">{props.title}</div>
+        <div
+          className="menu-item-status"
+          style={{ backgroundColor: props.color }}
+        >
+          {props.status}
+        </div>
+      </div>
+      <div className="menu-item-description">{props.description}</div>
+      {/* <div className="menu-item-breakdown">{props.breakdown}</div> */}
+      <div className="menu-item-demo">
+        {props.src !== "" ? (
+          <a
+            href={props.src}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="link"
+          >
+            {props.demo}
+          </a>
+        ) : (
+          <div>{props.demo}</div>
+        )}
+      </div>
     </div>
   );
 }
